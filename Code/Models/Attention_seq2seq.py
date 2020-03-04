405f0d84259ac2101c577307c5b08e3b57d6d009
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-04 21:33:49 +0000

    debugging

--- Code/Models/Attention_seq2seq.py
+++ Code/Models/Attention_seq2seq.py
@@ -168,7 +168,7 @@ class _Attention(nn.Module):
 
         # repeat decoder hidden state enc_seq_len times
         hidden = hidden.unsqueeze(1).repeat(1, enc_seq_len, 1)
-
+        print(encoder_outputs[0].shape)
         encoder_outputs = encoder_outputs[0].permute(1, 0, 2)
 
         # hidden = [batch size, enc_seq_len, dec hid dim]
