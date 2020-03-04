fe7c03396ec38833876f2265ffb0c6a3533f48b5
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-04 12:30:53 +0000

    debugging

--- Code/Models/generator_training_class.py
+++ Code/Models/generator_training_class.py
@@ -346,7 +346,7 @@ class generator:
         numericalVec_target = np.array(
             [[self.__word2index__(word) for word in sentence] for sentence in target]
             )
-        
+        return numericalVec_input, numericalVec_target
         ### Convert the input data to embedded representation
         max_lengths = np.array([len(sentence) for sentence in input]).max()
         embedded_matrix, input_seq_lengths = [], []
