304e6e77892a2c69447f118920bdc20eb28745c3
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-04 18:43:10 +0000

    debugging

--- Code/Models/generator_training_class.py
+++ Code/Models/generator_training_class.py
@@ -305,17 +305,17 @@ class generator:
             #dimension => [total_batchs, seq_length, batch_size, embed_dim], for target embed_dim is irrelevant
                 #seq_length is variable throughout the batches
         input_batches = np.array(
-            np.split(padded_input[:, (n_batches * self.batch_size):, :], n_batches, axis = 1)
+            np.split(padded_input[:, :(n_batches * self.batch_size), :], n_batches, axis = 1)
             )
         target_batches = np.array(
-            np.split(padded_target[:, (n_batches * self.batch_size):], n_batches, axis = 1)
+            np.split(padded_target[:, :(n_batches * self.batch_size)], n_batches, axis = 1)
             )
         # Split input and target lenghts into batches as well
         input_lenghts = np.array(
-            np.split(input_lengths[(n_batches * self.batch_size):], n_batches, axis = 0)
+            np.split(input_lengths[:(n_batches * self.batch_size)], n_batches, axis = 0)
             )
         target_lenghts = np.array(
-            np.split(target_lengths[(n_batches * self.batch_size):], n_batches, axis = 0)
+            np.split(target_lengths[:(n_batches * self.batch_size)], n_batches, axis = 0)
             )
         
         # trim sequences in individual batches
