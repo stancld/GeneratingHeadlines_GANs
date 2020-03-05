8ca0901738772ddd238b8094eee5bfff0f7391cb
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-05 13:03:25 +0000

    debugging

--- Code/Models/Attention_seq2seq.py
+++ Code/Models/Attention_seq2seq.py
@@ -346,6 +346,7 @@ class _Seq2Seq(nn.Module):
         """
         return np.array(
             [np.c_[np.ones((1, i)), np.zeros((1, i-input_lengths.max() - i))].reshape(-1) for i in input_lengts]
+            )
 
     def forward(self, seq2seq_input, input_lengths, target, teacher_forcing_ratio=0.5):
         """
