053a67f7c2e2998a0704d9ca00ea8acf3702812c
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-03-05 14:20:06 +0000

    debugging

--- Code/Models/Attention_seq2seq.py
+++ Code/Models/Attention_seq2seq.py
@@ -418,6 +418,7 @@ class _Seq2Seq(nn.Module):
             # if teacher forcing, use actual next token as next input
             # if not, use predicted token
             dec_input = target[t] if teacher_force else top1
+            return top1, dec_input
             dec_input = dec_input.squeeze(0).float().cpu()
         return outputs
 
